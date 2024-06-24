276da45407535c8bdfdf1db020b47429b3d310da
ABM1204 <bekzat.melisov@iaau.edu.kg>
2024-06-24 18:33:45 +0600

    Добавил функцию для удаления, подкорректировал шаблоны

--- source/to_do_list/models.py
+++ source/to_do_list/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [('new', 'New'), ('in_progress', 'In process'), ('done', 'Done'),]
+
+    description = models.CharField(max_length=200, verbose_name='Description')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new', verbose_name='Status')
+    due_date = models.DateField(null=True, blank=True, verbose_name='Done date')
+
+    def __str__(self):
+        return self.description
--- source/to_do_list/views.py
+++ source/to_do_list/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseNotFound
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'index.html', {'tasks': tasks})
+
+
+def add_task(request):
+    if request.method == 'POST':
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        due_date = request.POST.get('due_date')
+
+        if description:
+            task = Task(description=description, status=status, due_date=due_date)
+            task.save()
+            return redirect('task_list')
+        else:
+            return HttpResponseNotFound('<h1>ERROR: no description</h1>')
+
+    return render(request, 'add_task.html')
+
+
+def delete_task(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        return HttpResponseNotFound('<h1>Task not found</h1>')
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+
+    return render(request, 'delete.html', {'task': task})
