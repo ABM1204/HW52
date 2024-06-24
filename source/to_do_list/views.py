187f682d750d93ddc4056b4050ae3f7d066c4c03
ABM1204 <bekzat.melisov@iaau.edu.kg>
2024-06-24 18:42:45 +0600

    Добавил методы валидации и обработок ошибок при добавлении задачи

--- source/to_do_list/views.py
+++ source/to_do_list/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseBadRequest, HttpResponseNotFound
 from .models import Task
 
 def task_list(request):
@@ -13,12 +13,26 @@ def add_task(request):
         status = request.POST.get('status')
         due_date = request.POST.get('due_date')
 
-        if description:
-            task = Task(description=description, status=status, due_date=due_date)
-            task.save()
-            return redirect('task_list')
-        else:
-            return HttpResponseNotFound('<h1>ERROR: no description</h1>')
+
+        if not description:
+            return HttpResponseBadRequest('<h1>ERROR: Description is required</h1>')
+
+        if status not in ['new', 'in_progress', 'done']:
+            return HttpResponseBadRequest('<h1>ERROR: Invalid status</h1>')
+
+
+        from datetime import datetime
+        if due_date:
+            try:
+                due_date = datetime.strptime(due_date, '%Y-%m-%d').date()
+                if due_date < datetime.today().date():
+                    return HttpResponseBadRequest('<h1>ERROR: Due date cannot be in the past</h1>')
+            except ValueError:
+                return HttpResponseBadRequest('<h1>ERROR: Invalid date format</h1>')
+
+        task = Task(description=description, status=status, due_date=due_date)
+        task.save()
+        return redirect('task_list')
 
     return render(request, 'add_task.html')
 
